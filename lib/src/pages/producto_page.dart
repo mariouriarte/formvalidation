d408c24de1b484861363329bef353eb3d46af2e4
Mario Uriarte <mario.uriarte@gmail.com>
2020-09-03 16:59:24 -0400

    BLOC en productos terminado

--- lib/src/pages/producto_page.dart
+++ lib/src/pages/producto_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-
-import 'package:formvalidation/src/models/producto_model.dart';
-import 'package:formvalidation/src/providers/productos_provider.dart';
-import 'package:formvalidation/src/utils/utils.dart' as utils;
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+
+import 'package:formvalidation/src/models/producto_model.dart';
+import 'package:formvalidation/src/utils/utils.dart' as utils;
+import 'package:formvalidation/src/bloc/provider.dart';
+//import 'package:formvalidation/src/providers/productos_provider.dart';
 
 class ProductoPage extends StatefulWidget {
   @override
@@ -14,7 +15,11 @@ class ProductoPage extends StatefulWidget {
 class _ProductoPageState extends State<ProductoPage> {
   final formKey = GlobalKey<FormState>();
   final scaffoldKey = GlobalKey<ScaffoldState>();
-  final productoProvider = new ProductosProvider();
+
+  //se utilizara ProductosBloc
+//  final productoProvider = new ProductosProvider();
+
+  ProductosBloc productosBloc;
 
   ProductoModel producto = new ProductoModel();
   bool _guardando = false;
@@ -22,6 +27,8 @@ class _ProductoPageState extends State<ProductoPage> {
 
   @override
   Widget build(BuildContext context) {
+    productosBloc = Provider.productosBloc(context);
+
     final ProductoModel prodData = ModalRoute.of(context).settings.arguments;
     if (prodData != null) {
       producto = prodData;
@@ -130,13 +137,13 @@ class _ProductoPageState extends State<ProductoPage> {
     });
 
     if (foto != null) {
-      producto.fotoUrl = await productoProvider.subirImagen(foto);
+      producto.fotoUrl = await productosBloc.subirFoto(foto);
     }
 
     if (producto.id == null) {
-      productoProvider.crearProducto(producto);
+      productosBloc.agregarProducto(producto);
     } else {
-      productoProvider.editarProducto(producto);
+      productosBloc.editarProducto(producto);
     }
 
     setState(() {
